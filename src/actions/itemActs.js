c5ae8aefa0d8a42bcea6fcec2b5521d793e05491
scentinel <78578757+parfumGIT@users.noreply.github.com>
2021-02-12 01:31:33 +0000

    mercury

--- src/actions/itemActs.js
+++ src/actions/itemActs.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { GET_ITEMS, GET_ITEM, ADD_ITEM, EDIT_ITEM, DEL_ITEM, ITEMS_LOADING } from './types';
+const HOST = process.env.REACT_APP_API_HOST;
 
 export const getItems = () => dispatch => {
     console.log("GET_ITEMS ACTION")
